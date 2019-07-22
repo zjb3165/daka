6335b9ce1750dda0106eb5dde1b01ff891db4ef8
Swordwave Jumper <zjb3165@gmail.com>
2019-07-22 08:58:18 +0800

    添加系统管理员

--- database/migrations/2019_07_22_005222_create_admins_table.php
+++ database/migrations/2019_07_22_005222_create_admins_table.php
@@ -15,6 +15,10 @@ class CreateAdminsTable extends Migration
     {
         Schema::create('admins', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('username', 20)->unique();
+            $table->string('password', 100);
+            $table->string('name', 40);
+            $table->text('permissions');
             $table->timestamps();
         });
     }
---
+++ database/seeds/AdminSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\Admin;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $admin = new Admin();
+        $admin->username = 'root';
+        $admin->password = bcrypt('jumper@daka');
+        $admin->name = '系统管理员';
+        $admin->permissions = [];
+        $admin->save();
+    }
+}
